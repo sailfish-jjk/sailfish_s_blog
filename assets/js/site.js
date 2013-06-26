36cbce794cc33db2cc54484a18b9e4c64767b18a
dylan <dylanvivi@gmail.com>
2013-06-26 11:37:10 +0800

    update

--- assets/js/site.js
+++ assets/js/site.js
@@ -9,78 +9,118 @@
 var site = {
 	InitMap: {
 		list: function () {
-			elf('#List>.article>h2').on('click', site.Handlers.loadArticle);
+			elf(window).on('scroll', site.Handlers.scrolling);
 		},
 		
 		post: function () {
-			var disqusUrl = site.Urls.DISCUS_COMMENT;
+			var disqusUrl = site.URL_DISCUS_COMMENT;
 			disqusUrl && elf().loadScript(disqusUrl, {});
 		},
 		
 		search: function () {
-			var googleCseId = site.Vars.GOOGLE_CUSTOM_SEARCH_ID;
-			googleCseId && elf().loadScript(site.Urls.GOOGLE_API, {
-				onload: function () {
-					google.load('search', '1', {
-						language: 'zh-CN',
-						style: google.loader.themes.V2_DEFAULT,
-						callback: function() {
-							var customSearchControl = new google.search.CustomSearchControl(googleCseId, {});
-							customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
-							
-							var options = new google.search.DrawOptions();
-							options.setAutoComplete(true);
-							customSearchControl.draw('cse', options);
-							
-							var url = new elf.URL(location);
-							var query = url.getParameter('q');
-							if (query) {
-								query = decodeURIComponent(query);
-								document.title = elf().template(
-									site.Text.TPL_SEARCH_TITLE,
-									site.Vars.SITE_NAME,
-									query
-								);
-								customSearchControl.execute(query);
-							}
-						}
-					});
-				}
+			site.URL_GOOGLE_API &&
+			site.VAR_GOOGLE_CUSTOM_SEARCH_ID &&
+			elf().loadScript(site.URL_GOOGLE_API, {
+				onload: site.Handlers.onGCSEAPILoad
 			});
 		}
 	},
 	
 	Handlers: {
-		loadArticle: function (ev) {
-			var target = ev.target,
-				item = elf(target).parent();
-			if (target.nodeName != 'A' && item.attr('data-loaded') != 1) {
-				elf().ajax({
-					url: target.firstChild.getAttribute('href'),
-					onsuccess: site.Handlers.showAjaxContent.bind(item)
-				});
-			}
-			target = null;
+		deferLoad: function () {
+			elf('article').toArray()
+				.filter(site.Util.isViewable)
+				.filter(function (item) {
+					return item.getAttribute('content-loaded') != 1;
+				}).slice(0, 2).forEach(site.Handlers.loadArticle);
+			
 		},
 		
-		showAjaxContent: function (response) {
-			var content = response.split('<p class="article-meta">')[1].split('</p>');
+		loadArticle: function (item) {
+			elf().ajax({
+				url: elf(item).firstChild().firstChild().attr('href'),
+				onsuccess: function (response) {
+					site.Handlers.showAjaxContent(item, response);
+				}
+			});
+		},
+		
+		showAjaxContent: function (node, response) {
+			var article = elf(node);
+			var content = response.split('<p class="meta">')[1].split('</p>');
 			content.shift();
-			content = content.join('</p>').split(/<\/div>\s*<div id="disqus_thread" class="doc-comments">/)[0];
-			this.query('>div.article').html(content);
-			this.attr('data-loaded', 1);
+			content = content.join('</p>');
+			content = content.split(/<\/article>/)[0];
+			article.query('>.article-content').html(content);
+			article.attr('content-loaded', 1);
+			article.query('pre').forEach(function (item) {
+				hljs.highlightBlock(item);
+			});
+		},
+		
+		scrolling: function () {
+			var timer = site.scrollingTimer;
+			if (timer) {
+				clearTimeout(timer);
+			}
+			site.scrollingTimer = setTimeout(site.Handlers.deferLoad, 1000);
+		},
+		
+		onGCSEAPILoad: function () {
+			google.load('search', '1', {
+				language: 'zh-CN',
+				style: google.loader.themes.V2_DEFAULT,
+				callback: site.Handlers.onGCSEReady
+			});
+		},
+		
+		onGCSEReady: function() {
+			var customSearchControl = new google.search.CustomSearchControl(site.VAR_GOOGLE_CUSTOM_SEARCH_ID, {});
+			customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+			
+			var options = new google.search.DrawOptions();
+			options.setAutoComplete(true);
+			customSearchControl.draw('cse', options);
+			
+			var url = new elf.URL(location);
+			var query = url.getParameter('q');
+			if (query) {
+				query = decodeURIComponent(query);
+				document.title = elf().template(
+					site.TPL_SEARCH_TITLE,
+					site.VAR_SITE_NAME,
+					query
+				);
+				customSearchControl.execute(query);
+			}
 		}
 	},
 	
-	Text: {
-		TPL_SEARCH_TITLE: '#{0} / 搜索：#{1}'
-	},
-	
-	Vars: {},
-	
-	Urls: {}
+	Util: {
+		
+		isViewable: function (element) {
+			var pos = element.getBoundingClientRect();
+			var doc = js.dom.Stage.getDocumentElement();
+			var winHeight = doc.clientHeight;
+			var winWidth = doc.clientWidth;
+			var scrollLeft = document.body.scrollLeft || doc.scrollLeft;
+			var scrollTop = document.body.scrollTop || doc.scrollTop;
+			
+			return (pos.right > 0 &&
+				pos.left < winWidth &&
+				pos.bottom > 0 &&
+				pos.top < winHeight);
+		}
+	}
 };
 
 
-
-
+elf(function () {
+	hljs.initHighlighting();
+	
+	var module = document.body.className.replace(/page-type-/g, '').split(' ');
+	module.forEach(function (item) {
+		var initer = site.InitMap[item];
+		initer && elf(initer);
+	});
+});
